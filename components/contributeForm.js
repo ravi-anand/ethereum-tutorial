345243d860329934d951e955f1c41b27197dd132
Ravi <ravianand.anand72@gmail.com>
2019-05-04 15:07:47 +0530

    Request creation and view request functionality.

--- components/contributeForm.js
+++ components/contributeForm.js
@@ -2,30 +2,40 @@ import React, { Component } from 'react';
 import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Campaign from '../ethereum/campaign';
 import web3 from '../ethereum/web3';
+import { Router } from '../routes';
 
 class ContributionForm extends Component {
   state = {
-    value: ''
+    value: '',
+    errorMessage:'',
+    loading: false
   };
 
   onSubmit = async event => {
     event.preventDefault();
 
     const campaign = Campaign(this.props.address);
+
+    this.setState({ loading: true, errorMessage: '' });
+
     try {
       const accounts = await web3 .eth.getAccounts();
       await campaign.methods.contribute().send({
         from: accounts[0],
         value: web3.utils.toWei(this.state.value, 'ether')
       });
-    } catch (err) {
 
+      Router.replaceRoute(`/campaigns/${this.props.address}`);
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
     }
+
+    this.setState({ loading: false, value: '' });
   };
 
   render() {
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
         <Form.Field>
           <label>Amount to Contribute</label>
           <Input
@@ -35,7 +45,8 @@ class ContributionForm extends Component {
             labelPosition="right"
           />
         </Form.Field>
-        <Button  primary>Contribute!</Button>
+        <Message error header="Oops!" content={this.state.errorMessage} />
+        <Button  primary loading={ this.state.loading }>Contribute!</Button>
       </Form>
     );
   }
